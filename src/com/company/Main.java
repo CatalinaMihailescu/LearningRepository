6f1e7bcb498a4022eced06eb9699f33294bf6f88
CatalinaMihailescu <cagrigore@deloittece.com>
2023-07-17 15:11:00 +0300

    first commit from intellij

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int answer = 7 + 3 * 4;
+        System.out.println(answer);
+
+        double radius = 12.0;
+        double area = Math.PI * radius * radius;
+
+        int x = 3;
+        int y = x++;
+        System.out.printf(String.valueOf(x));
+    }
+}
