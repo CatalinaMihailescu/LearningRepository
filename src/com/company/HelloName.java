aef96ba564fd8f6152011b44108308e2aeacf094
CatalinaMihailescu <cagrigore@deloittece.com>
2023-07-19 13:00:07 +0300

    This is the hello name exercise

---
+++ src/com/company/HelloName.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Ex {
+
+    public static void main(String args[]) {
+        Scanner my_scan = new Scanner(System.in);
+        String my_str = my_scan.nextLine();
+        System.out.println("Hello " + my_str +"!");
+        }
+
+    }
+
+
---
+++ src/com/company/makeOutWord.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SubString {
+    public static void main(String args[]){
+        String out="Hello";
+        String word="Word";
+        System.out.println(out.substring(0,2) + word + out.substring(2));
+    }
+}
