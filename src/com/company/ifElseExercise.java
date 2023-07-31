16f4dcc765872231f59d880d20b4ff4fd19dcc0f
CatalinaMihailescu <cagrigore@deloittece.com>
2023-07-31 10:44:22 +0300

    new branch monday 31 july

---
+++ src/com/company/ifElseExercise.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ifElseExercise {
+        public static void main (String[] args){
+            Object obj1 = new Object();
+            Object obj2 = obj1;
+            if( obj1.equals(obj2) )
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
