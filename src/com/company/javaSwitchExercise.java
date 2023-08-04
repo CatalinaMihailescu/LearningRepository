b88d0e5cb4ba756548288482a1eb4a0600a0a543
CatalinaMihailescu <cagrigore@deloittece.com>
2023-08-04 13:30:55 +0300

    4th of aug

---
+++ src/com/company/javaSwitchExercise.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class javaSwitchExercise {
+    public void switchString(String input) {
+        switch (input) {
+            case "a":
+                System.out.println("apple");
+            case "b":
+                System.out.println("bat");
+                break;
+            case "B":
+                System.out.println("big bat");
+            default:
+                System.out.println("none");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        javaSwitchExercise tc = new javaSwitchExercise();
+        tc.switchString("B");
+    }
+}
+
+
